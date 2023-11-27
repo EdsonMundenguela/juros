e49cc4efd70def5b4cc33f19120139ba36ee7bf6
EdsonMundenguela <edsonthepale@gmail.com>
2023-11-27 13:59:08 -0300

    commit inicial

---
+++ juros.py
@@ -0,0 +1,15 @@
+# calculadora soma simples.
+#Apresentação,
+print('\n\t\t\t -- Calculadora de juros simples --\n')
+
+#Entrada
+n1 = float(input('informe N1'))
+n2 = float(input('Informe N2'))
+n3 = float(input('informe N3'))
+
+# processamento
+
+juros = n1*n2*n3 /100
+# saída
+
+print(f'{n1}*{n2}*{n3}/100={juros}')
